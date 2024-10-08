9013ea382e418d2eab903b7c5ce2d241a51c38a9
Mochi <beeyugo55@gmail.com>
2024-10-09 00:00:50 +0900

    パス被り

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,15 +1,15 @@
-// pages/work/[id].jsx
-import Link from "next/link";
-import Image from "next/image";
-import Head from "next/head";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import styles from "../styles/work.module.css";
-import { css } from "@emotion/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+// pages/work/[id].jsx 
+import Link from "next/link"; 
+import Image from "next/image"; 
+import Head from "next/head"; 
+import Header from "../components/Header"; 
+import Footer from "../components/Footer"; 
+import styles from "../styles/work.module.css"; 
+import { css } from "@emotion/react"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons"; 
 
-export default function WorkId({ work, previousWorks, nextWorks,icon,eventname }) {
+export default function WorkId({ work, previousWorks, nextWorks, icon, eventname }) { 
   const showComment = work.comment !== undefined && work.comment !== "";
   const showIcon = work.icon !== undefined && work.icon !== "";
   const showCreator = work.creator !== undefined && work.creator !== "";
@@ -26,211 +26,205 @@ export default function WorkId({ work, previousWorks, nextWorks,icon,eventname }
   const formattedDate = modifiedDate.toLocaleString();
   const showTime = work.time !== undefined && work.time !== "";
 
-  return (
-    <div>
-      <Head>
-        <title>
-          {work.title} - {work.creator} - オンライン映像イベント / PVSF archive
-        </title>
-        <meta
-          name="description"
-          content={`PVSFへの出展作品です。  ${work.title} - ${work.creator}`}
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@pvscreeningfes" />
-        <meta name="twitter:creator" content="@coroke3" />
-        <meta property="og:url" content="pvsf.jp" />
-        <meta
-          property="og:title"
-          content={`${work.title} - ${work.creator} / PVSF archive`}
-        />
-        <meta
-          property="og:description"
-          content={`PVSF 出展作品  ${work.title} - ${work.creator}  music:${work.music} - ${work.credit}`}
-        />
-        <meta property="og:image" content={work.largeThumbnail} />
-      </Head>
-      <Header />
-      <div className={styles.contentr}>
-        <div className={styles.bf}>
-          <div className={styles.s1f}>
-            <iframe
-              src={`https://www.youtube.com/embed/${work.ylink.slice(
-                17,
-                28
-              )}?vq=hd1080&autoplay=1`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-              className={styles.yf}
-            ></iframe>
-            <h1 className={styles.title}>{work.title}</h1>
-            <div className={styles.userinfo}>
-              {showIcon && work.icon ? (
-                <Link href={`../user/${work.tlink}`}>
-                  <Image
-                    src={`https://lh3.googleusercontent.com/d/${work.icon.slice(
-                      33
-                    )}`}
-                    className={styles.icon}
-                    alt={`${work.creator}のアイコン`}
-                    width={50}
-                    height={50}
-                  />{" "}
-                </Link>
-              ) : (
-                <Link href={`../user/${work.tlink}`}>
-                  <Image
-                    src="https://i.gyazo.com/07a85b996890313b80971d8d2dbf4a4c.jpg"
-                    alt={`アイコン`}
-                    className={styles.icon}
-                    width={50}
-                    height={50}
-                  />{" "}
-                </Link>
-              )}
-              {showCreator && (
-                <h3 className={styles.creator}>
-                  <Link href={`../user/${work.tlink}`}>{work.creator} </Link>
-                  {showYoutube && (
-                    <a
-                      href={`${work.ylink}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon icon={faYoutube} />
-                    </a>
-                  )}
-                  {showTwitter && (
-                    <a
-                      href={`https://twitter.com/${work.tlink}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon icon={faXTwitter} />
-                    </a>
-                  )}
-                </h3>
-              )}
-              {showTime && <p className={styles.time}>{formattedDate}</p>}
-            </div>
-            <div className={styles.eventInfo}>
-              <Link href={`../../event/${work.eventname}`}>
-              {icon && (
-                <Image
-                  src={`https://lh3.googleusercontent.com/d/${icon.slice(
-                    33
-                  )}`} // 実際のアイコンのパスを指定
-                  alt={`${eventname}のアイコン`}
-                  className={styles.eventIcon}
-                  width={50}
-                  height={50}
-                />
-              )}
-              {eventname && <h4 className={styles.eventTitle}>{eventname}</h4>}
-              </Link>
-            </div>
-            {showMusic && (
-              <p>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: `楽曲:${work.music} - ${work.credit}<br> `,
-                  }}
-                />
-              </p>
-            )}
-            {showComment && (
-              <p>
-                <div dangerouslySetInnerHTML={{ __html: `${work.comment}` }} />
-              </p>
-            )}
-            {showMenber && (
-              <table className={styles.table}>
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Name</th>
-                    <th>ID</th>
-                    <th>LINK</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {work.member.split(/[,、，]/).map((username, index) => {
-                    const memberId = work.memberid
-                      .split(/[,、，]/)
-                      [index]?.trim();
-                    return (
-                      <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{username.trim()}</td>
-                        <td>{memberId ? `@${memberId}` : "-"}</td>
-                        <td>
-                          {memberId ? (
-                            <a
-                              href={`https://twitter.com/${memberId}`}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <FontAwesomeIcon
-                                icon={faXTwitter}
-                                className={styles.twitterIcon}
-                              />
-                            </a>
-                          ) : (
-                            "-"
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            )}
-          </div>
-          <div className={styles.s2f}>
-            <div className={styles.navLinks}>
-              {previousWorks.map((prevWork) => (
-                <div className={styles.ss1} key={prevWork.ylink.slice(17, 28)}>
-                  <div className={styles.ss12}>
-                    <Link href={`/${prevWork.ylink.slice(17, 28)}`}>
-                      <img
-                        src={prevWork.smallThumbnail}
-                        width={`100%`}
-                        alt={`${prevWork.title} - ${prevWork.creator} | PVSF archive`}
-                      />
-                    </Link>
-                  </div>
-                  <div className={styles.ss13}>
-                    <p className={styles.scc}>{prevWork.title}</p>
-                    <p className={styles.sc}>{prevWork.creator}</p>
-                  </div>
-                </div>
-              ))}
-              {nextWorks.map((nextWork) => (
-                <div className={styles.ss1} key={nextWork.ylink.slice(17, 28)}>
-                  <div className={styles.ss12}>
-                    <Link href={`/${nextWork.ylink.slice(17, 28)}`}>
-                      <img
-                        src={nextWork.smallThumbnail}
-                        width={`100%`}
-                        alt={`${nextWork.title} - ${nextWork.creator} | PVSF archive`}
-                      />
-                    </Link>
-                  </div>
-                  <div className={styles.ss13}>
-                    <p className={styles.scc}>{nextWork.title}</p>
-                    <p className={styles.sc}>{nextWork.creator}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+  return ( 
+    <div> 
+      <Head> 
+        <title> 
+          {work.title} - {work.creator} - オンライン映像イベント / PVSF archive 
+        </title> 
+        <meta 
+          name="description" 
+          content={`PVSFへの出展作品です。  ${work.title} - ${work.creator}`} 
+        /> 
+        <meta name="twitter:card" content="summary_large_image" /> 
+        <meta name="twitter:site" content="@pvscreeningfes" /> 
+        <meta name="twitter:creator" content="@coroke3" /> 
+        <meta property="og:url" content="pvsf.jp" /> 
+        <meta 
+          property="og:title" 
+          content={`${work.title} - ${work.creator} / PVSF archive`} 
+        /> 
+        <meta 
+          property="og:description" 
+          content={`PVSF 出展作品  ${work.title} - ${work.creator}  music:${work.music} - ${work.credit}`} 
+        /> 
+        <meta property="og:image" content={work.largeThumbnail} /> 
+      </Head> 
+      <Header /> 
+      <div className={styles.contentr}> 
+        <div className={styles.bf}> 
+          <div className={styles.s1f}> 
+            {work.ylink && ( 
+              <iframe 
+                src={`https://www.youtube.com/embed/${work.ylink.slice(17, 28)}?vq=hd1080&autoplay=1`} 
+                title="YouTube video player" 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowFullScreen 
+                className={styles.yf} 
+              ></iframe>)} 
+            <h1 className={styles.title}>{work.title}</h1> 
+            <div className={styles.userinfo}> 
+              {showIcon && work.icon ? ( 
+                <Link href={`../user/${work.tlink}`}> 
+                  <Image 
+                    src={`https://lh3.googleusercontent.com/d/${work.icon.slice(33)}`} 
+                    className={styles.icon} 
+                    alt={`${work.creator}のアイコン`} 
+                    width={50} 
+                    height={50} 
+                  />{" "} 
+                </Link> 
+              ) : ( 
+                <Link href={`../user/${work.tlink}`}> 
+                  <Image 
+                    src="https://i.gyazo.com/07a85b996890313b80971d8d2dbf4a4c.jpg" 
+                    alt={`アイコン`} 
+                    className={styles.icon} 
+                    width={50} 
+                    height={50} 
+                  />{" "} 
+                </Link> 
+              )} 
+              {showCreator && ( 
+                <h3 className={styles.creator}> 
+                  <Link href={`../user/${work.tlink}`}>{work.creator} </Link> 
+                  {showYoutube && ( 
+                    <a 
+                      href={`${work.ylink}`} 
+                      target="_blank" 
+                      rel="noopener noreferrer" 
+                    > 
+                      <FontAwesomeIcon icon={faYoutube} /> 
+                    </a> 
+                  )} 
+                  {showTwitter && ( 
+                    <a 
+                      href={`https://twitter.com/${work.tlink}`} 
+                      target="_blank" 
+                      rel="noopener noreferrer" 
+                    > 
+                      <FontAwesomeIcon icon={faXTwitter} /> 
+                    </a> 
+                  )} 
+                </h3> 
+              )} 
+              {showTime && <p className={styles.time}>{formattedDate}</p>} 
+            </div> 
+            <div className={styles.eventInfo}> 
+              {work.eventname && ( 
+                <Link href={`../../event/${work.eventname}`}> 
+                {icon && ( 
+                  <Image 
+                    src={`https://lh3.googleusercontent.com/d/${icon.slice(33)}`} 
+                    alt={`${eventname}のアイコン`} 
+                    className={styles.eventIcon} 
+                    width={50} 
+                    height={50} 
+                  /> 
+                )} 
+                {eventname && <h4 className={styles.eventTitle}>{eventname}</h4>} 
+                </Link> 
+              )} 
+            </div> 
+            {showMusic && ( 
+              <p> 
+                <div 
+                  dangerouslySetInnerHTML={{ 
+                    __html: `楽曲:${work.music} - ${work.credit}<br> `, 
+                  }} 
+                /> 
+              </p> 
+            )} 
+            {showComment && ( 
+              <p> 
+                <div dangerouslySetInnerHTML={{ __html: `${work.comment}` }} /> 
+              </p> 
+            )} 
+            {showMenber && ( 
+              <table className={styles.table}> 
+                <thead> 
+                  <tr> 
+                    <th>No</th> 
+                    <th>Name</th> 
+                    <th>ID</th> 
+                    <th>LINK</th> 
+                  </tr> 
+                </thead> 
+                <tbody> 
+                  {work.member.split(/[,、，]/).map((username, index) => { 
+                    const memberId = work.memberid.split(/[,、，]/)[index]?.trim(); 
+                    return ( 
+                      <tr key={index}> 
+                        <td>{index + 1}</td> 
+                        <td>{username.trim()}</td> 
+                        <td>{memberId ? `@${memberId}` : "-"}</td> 
+                        <td> 
+                          {memberId ? ( 
+                            <a 
+                              href={`https://twitter.com/${memberId}`} 
+                              target="_blank" 
+                              rel="noopener noreferrer" 
+                            > 
+                              <FontAwesomeIcon 
+                                icon={faXTwitter} 
+                                className={styles.twitterIcon} 
+                              /> 
+                            </a> 
+                          ) : ( 
+                            "-" 
+                          )} 
+                        </td> 
+                      </tr> 
+                    ); 
+                  })} 
+                </tbody> 
+              </table> 
+            )} 
+          </div> 
+          <div className={styles.s2f}> 
+            <div className={styles.navLinks}> 
+              {previousWorks.map((prevWork) => ( 
+                <div className={styles.ss1} key={prevWork.ylink.slice(17, 28)}> 
+                  <div className={styles.ss12}> 
+                    <Link href={`/${prevWork.ylink.slice(17, 28)}`}> 
+                      <img 
+                        src={prevWork.smallThumbnail} 
+                        width={`100%`} 
+                        alt={`${prevWork.title} - ${prevWork.creator} | PVSF archive`} 
+                      /> 
+                    </Link> 
+                  </div> 
+                  <div className={styles.ss13}> 
+                    <p className={styles.scc}>{prevWork.title}</p> 
+                    <p className={styles.sc}>{prevWork.creator}</p> 
+                  </div> 
+                </div> 
+              ))} 
+              {nextWorks.map((nextWork) => ( 
+                <div className={styles.ss1} key={nextWork.ylink.slice(17, 28)}> 
+                  <div className={styles.ss12}> 
+                    <Link href={`/${nextWork.ylink.slice(17, 28)}`}> 
+                      <img 
+                        src={nextWork.smallThumbnail} 
+                        width={`100%`} 
+                        alt={`${nextWork.title} - ${nextWork.creator} | PVSF archive`} 
+                      /> 
+                    </Link> 
+                  </div> 
+                  <div className={styles.ss13}> 
+                    <p className={styles.scc}>{nextWork.title}</p> 
+                    <p className={styles.sc}>{nextWork.creator}</p> 
+                  </div> 
+                </div> 
+              ))} 
+            </div> 
+          </div> 
+        </div> 
+      </div> 
+    </div> 
+  ); 
+} 
 
 export async function getStaticPaths() {
   try {
@@ -251,6 +245,11 @@ export async function getStaticPaths() {
         return null; // 重複する場合はnullを返す
       })
       .filter(Boolean); // nullを除去
+
+    // 重複パスがあった場合の処理
+    if (paths.length !== uniquePaths.size) {
+      console.warn("Duplicate paths detected. Some paths were skipped.");
+    }
 
     return {
       paths,
@@ -387,6 +386,7 @@ export async function getStaticProps({ params }) {
         (w) => w.ylink === ylink
       )
     );
+
     const eventId = work.eventname; // eventnameからイベントIDを取得
     const eventRes = await fetch(
       `https://script.google.com/macros/s/AKfycbybjT6iEZWbfCIzTvU1ALVxp1sa_zS_pGJh5_p_SBsJgLtmzcmqsIDRtFkJ9B8Yko6tyA/exec?eventid=${eventId}`
